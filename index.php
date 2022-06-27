5ad657f8d2a68077885c3ff923ab9744633b3aa2
Praveen Raj <pr@lbit.in>
2022-06-27 22:17:18 +0530

    Add note regarding multibyte string output

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 #!/bin/env php
 <?php
 
+// PLEASE NOTE: The multibyte characters in the internal and external
+// DB host names are being counted by var_dump as 2 characters. So the
+// string length in the output currently is 22 characters for "db­01­internal.local"
+// instead of 20. Similarly for "db­01.company.tld", it shows 18 instead of 17.
+
 // Main function call
 var_dump(parseConfigFile(__DIR__ . "/config.txt"));
 
